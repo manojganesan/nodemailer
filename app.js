e55839ba6ea4856668e6c83a8be69667e205e904
manojganesan <manoj3493@gmail.com>
2018-09-27 13:13:34 +0530

    mailer code commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var nodemailer = require('nodemailer');
+
+// Create the transporter with the required configuration for Outlook
+// change the user and pass !
+var transporter = nodemailer.createTransport({
+  
+    //host:"smtp.gmail.com",
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: true, // TLS requires secureConnection to be false
+    //port:465, //port for secure SMTP - SSL
+    port: 587, // port for secure SMTP - TLS
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'yourmail',
+        pass: 'yourpassword'
+    }
+});
+
+// setup e-mail data, even with unicode symbols
+var mailOptions = {
+    from: '"Our Code World " <your mail>', // sender address (who sends)
+    to: 'tomail@gmail.com', // list of receivers (who receives)
+    subject: 'Hello ', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+
+    console.log('Message sent: ' + info.response);
+});
